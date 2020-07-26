af81a5840ff3691afb964eea335f763304bf5152
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-26 11:02:25 -0400

    Update rev_per_week.sql

--- rev_per_week.sql
+++ rev_per_week.sql
@@ -5,4 +5,4 @@ SELECT DATE_TRUNC('week', order_date) :: DATE AS delivr_week,
 -- Keep only the records in June 2018
 WHERE DATE_TRUNC('month', order_date) = '2018-06-01'
 GROUP BY delivr_week
-ORDER BY delivr_week;
+ORDER BY delivr_week ASC;
